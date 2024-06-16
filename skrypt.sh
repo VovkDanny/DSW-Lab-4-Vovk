c6cea6687219b40bffc4449481e51ef8770261c3
VovkDanny <126683989+VovkDanny@users.noreply.github.com>
2024-06-16 19:30:55 +0200

    Update skrypt.sh,create logs100 function

--- skrypt.sh
+++ skrypt.sh
@@ -1,3 +1,17 @@
 if [ "$1" = "--date" ]; then
     date
+    fi
+    if [ "$1" = "--logs" ]; then
+    count=${2:-100}
+
+    mkdir -p logx
+
+    for ((i=1; i<=$count; i++)); do
+        echo "File: log$i.txt" > logx/log$i.txt
+        echo "Created by: $0" >> logx/log$i.txt
+        echo "Date: $(date)" >> logx/log$i.txt
+    done
+
+    echo "Created $count files in the logx directory."
+    exit 0
 fi
